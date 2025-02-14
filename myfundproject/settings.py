94dca47ae4a4ca237d0e0eafaf48097ec6172958
Samuel Ojedele <samuelojedele2003@gmail.com>
2025-02-14 13:34:46 +0100

    Group Buy Feature(Phase 1) Completed!

--- myfundproject/settings.py
+++ myfundproject/settings.py
@@ -212,6 +212,9 @@ else:
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
+            'OPTIONS': {
+            'timeout': 20,  # Increase timeout value (in seconds)
+        },
         }
     }
 
