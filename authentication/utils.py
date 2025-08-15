f2fbea4b79767d8be81ec5772bfd71c46fc1c314
Jubaida Tasnim <72998206+JTasnim@users.noreply.github.com>
2025-08-15 09:07:34 -0700

    Generic email (#39)
    * Generic email
    
    * search function for all transactions and deactivate the user account
    
    * Optimised Generic Emails
    
    ---------
    
    Co-authored-by: Samuel Ojedele <samuelojedele2003@gmail.com>

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,6 +1,14 @@
 import requests
 from .models import PushNotifications
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+import threading
+import logging
+
+# Set up logging (make sure logging is configured in your settings or app)
+logger = logging.getLogger(__name__)
 
 EXPO_PUSH_URL = "https://exp.host/--/api/v2/push/send"
 
@@ -60,3 +68,31 @@ def send_push_notification(user, title, message, data=None, notif_type="SYSTEM")
             print(f"🔥 Error sending to {token}: {str(e)}")
 
     return {"sent": success_count, "total": len(tokens)}
+
+
+def send_generic_email(subject, message, from_email="MyFund <info@myfundmobile.com>", recipient_list=[]):
+    def send_email_task():
+        try:
+            context = {
+                "subject": subject,
+                "message": message
+            }
+
+            html_message = render_to_string("email/email.html", context=context)
+            plain_message = strip_tags(html_message)
+
+            send_mail(
+                subject,
+                plain_message,
+                from_email,
+                recipient_list,
+                html_message=html_message
+            )
+            
+        except Exception as e:
+            logger.error(f"Failed to send email: {e}", exc_info=True)
+
+
+    # Start the background thread
+    thread = threading.Thread(target=send_email_task)
+    thread.start()
