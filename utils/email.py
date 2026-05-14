e464ff4ba9fbbf06d31f389e7ce2c713f9d1d5e1
Tolulope Ahmed <tolulopeahmed@gmail.com>
2026-05-14 08:03:07 +0100

    Fix Resend

--- utils/email.py
+++ utils/email.py
@@ -7,7 +7,7 @@ resend.api_key = os.environ.get("RESEND_API_KEY")
 def send_otp_email(user, otp):
     resend.Emails.send(
         {
-            "from": "MyFund <noreply@myfundmobile.com>",
+            "from": "MyFund <noreply@mg.myfundmobile.com>",
             "to": user.email,
             "subject": "Your MyFund OTP",
             "html": f"<h2>{otp}</h2><p>Your OTP code</p>",
